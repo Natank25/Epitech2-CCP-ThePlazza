c26c5a138c286a0af2855d6052f43193935e9225
Natan Pereira <natan.pereira@epitech.eu>
2026-05-11 14:08:30 +0200

    Initial commit

---
+++ include/plazza.hpp
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2026
+** Plazza
+** File description:
+** DESCRIPTION
+*/
+
+#ifndef PLAZZA_HPP
+    #define PLAZZA_HPP
+
+namespace plazza {
+    constexpr int EPI_SUCCESS = 0;
+    constexpr int EPI_FAILURE = 84;
+
+}
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2026
+** Plazza
+** File description:
+** DESCRIPTION
+*/
+
+#include "plazza.hpp"
+
+int main(int argc, char **argv)
+{
+    return plazza::EPI_SUCCESS;
+}
