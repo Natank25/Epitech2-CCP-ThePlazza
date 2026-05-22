a37ca3059f04ee934906ee1e5196afa8e1de863a
armand <armand.lecomte@epitech.eu>
2026-05-22 16:14:33 +0200

    Feat(assets): Added ingredients assets

--- include/Ingredient.hpp
+++ include/Ingredient.hpp
@@ -14,17 +14,25 @@ namespace plazza {
         std::string imagePath;
     };
 
-    static const std::string TOMATO_PATH{"Assets/Tomato.png"};
+    static const std::string TOMATO_PATH{"assets/Tomato.png"};
+    static const std::string CHEESE_PATH{"assets/Cheese.png"};
+    static const std::string DOUGH_PATH{"assets/Dough.png"};
+    static const std::string GRUYERE_PATH{"assets/Cheese.png"};
+    static const std::string HAM_PATH{"assets/Ham.png"};
+    static const std::string MUSHROOM_PATH{"assets/Mushrooms.png"};
+    static const std::string STEAK_PATH{"assets/Steak.png"};
+    static const std::string EGGPLANT_PATH{"assets/Eggplant.png"};
+    static const std::string GOAT_CHEESE_PATH{"assets/Goatcheese.png"};
 
-    static const Ingredient DOUGH = {"dough", std::string("TODO")};
-    static const Ingredient TOMATO = {"tomato", std::string(TOMATO_PATH)};
-    static const Ingredient GRUYERE = {"gruyere", std::string("TODO")};
-    static const Ingredient HAM = {"ham", std::string("TODO")};
-    static const Ingredient MUSHROOMS = {"mushrooms", std::string("TODO")};
-    static const Ingredient STEAK = {"steak", std::string("TODO")};
-    static const Ingredient EGGPLANT = {"eggplant", std::string("TODO")};
-    static const Ingredient GOAT_CHEESE = {"goat cheese", std::string("TODO")};
-    static const Ingredient CHIEF_LOVE = {"chief love", std::string("TODO")};
+    static const Ingredient DOUGH = {"dough", TOMATO_PATH};
+    static const Ingredient TOMATO = {"tomato", TOMATO_PATH};
+    static const Ingredient GRUYERE = {"gruyere", GRUYERE_PATH};
+    static const Ingredient HAM = {"ham", HAM_PATH};
+    static const Ingredient MUSHROOMS = {"mushrooms", MUSHROOM_PATH};
+    static const Ingredient STEAK = {"steak", STEAK_PATH};
+    static const Ingredient EGGPLANT = {"eggplant", EGGPLANT_PATH};
+    static const Ingredient GOAT_CHEESE = {"goat cheese", GOAT_CHEESE_PATH};
+    static const Ingredient CHIEF_LOVE = {"chief love", "TODO"};
 
 }
 
