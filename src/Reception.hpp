ac24bfcdb3be0ac5dc511950f21dabcb5693201d
Natan Pereira <natan.pereira@epitech.eu>
2026-05-22 10:28:50 +0200

    feat(Reception): Reception default class

---
+++ src/Reception.hpp
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2026
+** ThePlazza
+** File description:
+** DESCRIPTION
+*/
+
+#ifndef RECEPTION_HPP
+    #define RECEPTION_HPP
+
+    #include <string>
+
+class Reception {
+public:
+    void parseLine(const std::string &line);
+private:
+
+};
+#endif
