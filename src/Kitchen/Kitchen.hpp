c4f090dba1fd52a622ea6b20f9fee949e5cb6cff
armand <armand.lecomte@epitech.eu>
2026-05-27 01:36:27 +0200

    Feat(Kitchen): Added kitchen ctor & usefull functions & check inactivity to shutdown if not used after a certain delay

--- src/Kitchen/Kitchen.hpp
+++ src/Kitchen/Kitchen.hpp
@@ -7,6 +7,7 @@
 
 #ifndef KITCHEN_HPP
     #define KITCHEN_HPP
+    #include <functional>
     #include <queue>
 
     #include "Cook.hpp"
@@ -16,14 +17,36 @@
 namespace plazza {
     class Kitchen {
     public:
-        explicit Kitchen(std::size_t _numberCooks);
-    private:
-        void refill(double deltaTime);
+        Kitchen(int refillTime, double multiplier, std::size_t nbCooks);
 
-        double _timeSinceLastRefill;
-        std::vector<Cook> _cooks;
-        std::queue<PizzaOrder> _orders;
+        void shutdown();
+        void start();
+
+        void enqueue(const PizzaOrder &order);
+
+        bool isFull() const;
+        bool isRunning() const;
+
+        void setOnPizzaDone(std::function<void(const PizzaOrder &)> cb);
+        void stockRefillLoop();
+        void inactivityCheckLoop();
+
+        ~Kitchen();
+    private:
+        static constexpr int CHECK_ACTIVITY_INTERVAL_MS = 500;
+        static constexpr int INACTIVITY_CLOSE_TIME_S = 5;
+
+        int _refillTimeMs;
+        double _multiplier;
+        std::atomic<bool> _running;
+        std::atomic<std::chrono::steady_clock::time_point> _lastActivity;
+
         Stock _stock;
+        SafeQueue<PizzaOrder> _queue;
+        std::vector<Cook> _cooks;
+        std::vector<std::thread> _threads;
+
+        std::function<void(const PizzaOrder &)> _onPizzaDone;
     };
 } // namespace plazza
 
