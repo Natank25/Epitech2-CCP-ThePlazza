0699486b8502b7f9a783c06ddf83462414b3f8e7
Natan Pereira <natan.pereira@epitech.eu>
2026-05-28 13:39:38 +0200

    feat(Poller): Poller class

---
+++ src/Poller.hpp
@@ -0,0 +1,42 @@
+/*
+** EPITECH PROJECT, 2026
+** plazza
+** File description:
+** DESCRIPTION
+*/
+
+#ifndef POLLER_HPP
+    #define POLLER_HPP
+    #include <vector>
+    #include <sys/poll.h>
+
+namespace plazza {
+    class Poller {
+    public:
+        Poller();
+
+        void pushBack(int fd);
+
+        void popBack();
+
+        int poll(int timeout = -1);
+
+        const std::vector<int> &
+        getPollResult();
+
+        const std::vector<int> &
+        getDisconnectedFds();
+
+        void removeFromFd(int fd);
+
+    private:
+        std::vector<pollfd> createPollFds();
+
+        std::vector<int> _listeningFd;
+
+        std::vector<int> _pollResults;
+        std::vector<int> _disconnectedFds;
+    };
+} // plazza
+
+#endif
